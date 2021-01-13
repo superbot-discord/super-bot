42a042ad6560a314a0f894920fe18ab5dc14758e
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-13 13:04:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -268,12 +268,12 @@ async def speedtest(ctx):
   total=0
   now1 = datetime.now()
   message = await ctx.send("Pong!")
-  mcs = (datetime.now() - now1).microseconds+(datetime.now() - now1).seconds*1000000
+  mcs = (datetime.now() - now1).microseconds
   total = total + mcs
   for count in range(1,6):
     now1 = datetime.now()
     await message.edit(content="Pong! "+str(mcs)+" microseconds  (Test "+str(count)+")")
-    mcs = (datetime.now() - now1).microseconds+(datetime.now() - now1).seconds*1000000
+    mcs = (datetime.now() - now1).microseconds
     total = total + mcs
   avg = total/6
   await message.edit(content=f"Pong!\nTotal time: "+str(total)+f" mcs\nAverage time: "+str(avg)+" mcs")
