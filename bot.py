a39a7d24f7eb76f6b409cc4e5395b04845aa9316
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-14 09:25:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -320,26 +320,26 @@ async def ocr(ctx):
 @bot.command()
 async def text(ctx):
   files = ctx.message.attachments
-    for count in range(0,len(files)):
-      r = requests.get(files[count].url, stream=True)
-      r.raise_for_status()
-      r.raw.decode_content = True
-      if files[count].filename.endswith(".pdf"):
-        open('pdf.pdf', 'wb').write(r.content)
-        images = convert_from_path('pdf.pdf')
-        for count in images:
-          desc=pytesseract.image_to_string(count)
-        os.remove('pdf.pdf')
-      elif files[count].endswith(".txt")
-        open('txt.txt', 'wb').write(r.content)
-        with open('data.txt', 'r') as file:
-          desc = file.read().replace('\n', '')
-        os.remove('txt.txt')
-      else:
-        desc = "Unsupported format. Please use .pdf or .txt."
-      if desc=="":
-        desc="There was no text."
-      await ctx.send(desc)
+  for count in range(0,len(files)):
+    r = requests.get(files[count].url, stream=True)
+    r.raise_for_status()
+    r.raw.decode_content = True
+    if files[count].filename.endswith(".pdf"):
+      open('pdf.pdf', 'wb').write(r.content)
+      images = convert_from_path('pdf.pdf')
+      for count in images:
+        desc=pytesseract.image_to_string(count)
+      os.remove('pdf.pdf')
+    elif files[count].endswith(".txt")
+      open('txt.txt', 'wb').write(r.content)
+      with open('data.txt', 'r') as file:
+        desc = file.read().replace('\n', '')
+      os.remove('txt.txt')
+    else:
+      desc = "Unsupported format. Please use .pdf or .txt."
+    if desc=="":
+      desc="There was no text."
+    await ctx.send(desc)
 
 @bot.command()
 async def html(ctx, *, code = None)
