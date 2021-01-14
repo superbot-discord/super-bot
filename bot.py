582ec04074d20f1ca845efa78a1eed57de3a6847
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-14 17:03:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -358,7 +358,7 @@ async def youtube(ctx, *, url):
   os.mkdir("Video")
   youtube = pytube.YouTube(url)
   video = youtube.streams.get_highest_resolution()
-  video.download("/Video")
+  video.download("/working/Video")
   shutil.make_archive("Video.zip", 'zip', "Video")
   await ctx.send(file=discord.File('Video.zip'))
   shutil.rmtree('Video')
