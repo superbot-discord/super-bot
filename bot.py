59e49c806550c90a927e08e6ae85163230806457
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-15 13:11:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -356,11 +356,11 @@ async def html(ctx, *, code = None):
 @bot.command()
 async def youtube(ctx, *, url):
   youtube = pytube.YouTube(url)
-  video = youtube.streams.get_highest_resolution()
+  video = youtube.streams.filter(file_extension='mp4').get_highest_resolution()
   if video.filesize<8388119:
     video.download(filename='YTVideo')
   else:
-    for count in youtube.streams.filter(subtype='mp4').order_by('resolution').desc():
+    for count in youtube.streams.filter(file_extension='mp4').order_by('resolution').desc():
       if count.filesize<8388119:
         count.download(filename='YTVideo')
         break
@@ -377,7 +377,7 @@ async def youtube(ctx, *, url):
     await ctx.send(file=discord.File('captions.txt'))
     os.remove('captions.txt')
   except:
-    await msg.edit("No captions available for the video.")
+    await edit("No captions available for the video.")
   os.remove('YTVideo.mp4')
 
 @bot.command()
