2e9819ac9ae9889fc5594e09332a85b5922da19b
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-15 09:15:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ import os
 from pdf2image import convert_from_path
 import imgkit
 import pytube
-from moviepy import *
+from moviepy.editor import *
 
 hexstring_pattern = re.compile(r'#?([0-9A-F]{2})([0-9A-F]{2})([0-9A-F]{2})', re.IGNORECASE)
 intents = discord.Intents.all()
