67c388ab8a1b7e5fb39e9d595ced7a660cb659d1
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-19 12:57:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -406,16 +406,16 @@ async def wiki(ctx, *, query):
     if len(desc)>2048:
       desc = desc[0:2046]+"…"
     page = wikipedia.page(title=query, auto_suggest=True, redirect=True, preload=False)
+    embed = discord.Embed(title=query, description=desc)
+    for count in page.sections:
+      embed.add_field(name=count, value=wikipeida.section(count)[:500], inline=False)
+    if len(page.images)!=0:
+      embed.set_thumbnail(url = page.images[0])
   except:
     results = wikipedia.search(query, results=20, suggestion=False)
     desc = "**Please make one of these searches:**"
     for count in results:
       desc = desc + "`"+str(count)+"` "
-  embed = discord.Embed(title=query, description=desc)
-  for count in page.sections:
-    embed.add_field(name=count, value=wikipeida.section(count)[:500], inline=False)
-  if len(page.images)!=0:
-    embed.set_thumbnail(url = page.images[0])
   await ctx.send(embed = embed)
 
 @bot.command()
