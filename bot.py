5341af174d8511c5218b1b0e6ecaee8f42598b53
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-21 11:17:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -798,8 +798,10 @@ async def rtimer(ctx, timetocount,*,Text=None):
       newhrs=newhrs.replace("8",":eight: ")
       newhrs=newhrs.replace("9",":nine: ")
       newhrs=newhrs.replace("0",":zero: ")
+      prevdesc = desc
       desc=newhrs+" :regional_indicator_h: "+newmin+" :regional_indicator_m: "+newsec+":regional_indicator_s:"
-      await message.edit(content = desc)
+      if desc != prevdesc:
+        await message.edit(content = desc)
     await message.edit(content="Countdown complete!")
     if Text==None:
       await message.reply("Countdown complete!")
