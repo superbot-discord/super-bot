a27d756e83fdbd96a522738ed3f2eee2779709b8
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-22 20:45:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -647,7 +647,7 @@ async def transparent(ctx, alpha = 50):
   await ctx.message.attachments[0].save("Not_Transparent.png")
   img = PIL.Image.open("Not_Transparent.png")
   img2 = img.copy()
-  img2.putalpha(int(alpha))
+  img2.putalpha(float(alpha))
   img.paste(img2, img)
   img.save('Transparent.png')
   await ctx.send(file = discord.File('Transparent.png'))
