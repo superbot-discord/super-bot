5f6dd8217436cd9141d4a1199867905018b5a276
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-22 17:31:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -645,8 +645,8 @@ async def engrave(ctx, product, *, text):
 async def transparent(ctx, alpha):
   await ctx.message.attachments[0].save("Not_Transparent.png")
   img = PIL.Image.open("Not_Transparent.png")
-  PIL.newimg.putalpha(int(alpha))
-  PIL.newimg.save('Transparent.png')
+  newimg.putalpha(int(alpha))
+  newimg.save('Transparent.png')
   await ctx.send(file = discord.File('Transparent.png'))
 
 @bot.command()
