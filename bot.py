8d00cd43cbc9a0eb9e73e5622df1e2d0c6de3146
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-01-29 15:41:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1041,7 +1041,35 @@ async def embed(ctx,*,text):
     textlist.remove(textlist[0])
     textlist.remove(textlist[0])
   await ctx.send(embed=embed)
-  
+
+@bot.command()
+async def editembed(ctx, message : discord.Message, *,text):
+  textlist=text.splitlines()
+  embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"), color=int(textlist[3]))
+  textlist.remove(textlist[0])
+  textlist.remove(textlist[0])
+  textlist.remove(textlist[0])
+  textlist.remove(textlist[0])
+  embed.set_author(name=textlist[0], url=textlist[1], icon_url=textlist[2])
+  textlist.remove(textlist[0])
+  textlist.remove(textlist[0])
+  textlist.remove(textlist[0])
+  embed.set_footer(text=textlist[0])
+  textlist.remove(textlist[0])
+  embed.set_thumbnail(url=textlist[0])
+  textlist.remove(textlist[0])
+  embed.set_image(url=textlist[0])
+  textlist.remove(textlist[0])
+  for count in range(0,len(textlist)//3):
+    if textlist[2].lower()=="y" or textlist[2].lower()=="yes" or textlist[2].lower()=="true" or textlist[2].lower()=="1":
+      inl=True
+    else:
+      inl=False
+    embed.add_field(name=textlist[0], value=textlist[1].replace("{{{newline}}}","\n"), inline=inl)
+    textlist.remove(textlist[0])
+    textlist.remove(textlist[0])
+    textlist.remove(textlist[0])
+  await message.edit(embed=embed)
 
 @bot.command()
 async def insert(ctx,emoji,*,text):
