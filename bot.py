c6d2799d1e9a1f35188f15e0a42fa765e292777a
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-01 12:35:10 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -597,18 +597,21 @@ async def python(ctx, *, script):
   file.write(script)
   file.close()
   proc = subprocess.Popen(['python', 'program.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  output = str(proc.communicate()[0])
-  output = output.lstrip("'b").rstrip("\\n'").replace('\\n', f"\n")
-  outputlist = output.splitlines()
-  if len(outputlist)==0:
-    await ctx.send("There was no result.")
-  elif len(outputlist)<10:
-    await ctx.send(f"```\n"+output+f"\n```")
-  else:
-    truncatedoutput = ""
-    for count in range(0,11):
-      truncatedoutput = truncatedoutput + outputlist[count] + f"\n"
-    await ctx.send(f"The result was truncated due to the length of the result.\n```\n"+truncatedoutput+f"\n```")
+  try:
+    output = str(proc.communicate(timeout = 5)[0])
+    output = output.lstrip("'b").rstrip("\\n'").replace('\\n', f"\n")
+    outputlist = output.splitlines()
+    if len(outputlist)==0:
+      await ctx.send("There was no result.")
+    elif len(outputlist)<10:
+      await ctx.send(f"```\n"+output+f"\n```")
+    else:
+      truncatedoutput = ""
+      for count in range(0,11):
+        truncatedoutput = truncatedoutput + outputlist[count] + f"\n"
+      await ctx.send(f"The result was truncated due to the length of the result.\n```\n"+truncatedoutput+f"\n```")
+  except:
+    await ctx.send("The script timed out.")
   os.remove("program.py")
 
 @bot.command()
