efe7f09621c67163247413b5d9d3020c912e899a
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-05 09:47:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -516,10 +516,10 @@ async def translate(ctx, langinput = "list", *, text = "Sample text"):
           await msg.edit(content = "**Translation from "+langdict[fromlang]+" to "+langdict[lang]+f":**\n"+translation.text.replace("u003c", "<").replace("u003e", ">").replace("u0026", "&"))
         except:
           await msg.edit(content = "**Translation from "+langdict[fromlang]+" to "+langdict[lang]+f":**\n"+translation.text.replace("u003c", "<").replace("u003e", ">").replace("u0026", "&"))
-      except:
+    except NameError:
         translation = translatorvar.translate(text, dest=lang)
         await msg.edit(content = "**Translation from "+langdict[translatorvar.detect(text).lang]+" to "+langdict[lang]+f":**\n"+translation.text.replace("u003c", "<").replace("u003e", ">").replace("u0026", "&"))
-    except:
+    else:
       await ctx.send("Language not found! Please use `=translate list` to get a list of languages.")
 
 @bot.command()
