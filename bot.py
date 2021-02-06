d92f217ac4d739f858aca8664fa6907f8ee2fa5d
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-06 22:49:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1029,8 +1029,8 @@ async def wiki(ctx, *, query):
   embed = discord.Embed(title=query, url="https://en.wikipedia.org/wiki/"+wpage.title.replace(" ","_"), description=desc)
   print(wpage.sections)
   for count in wpage.sections[:2]:
-    embed.add_field(name=count, value=wikipedia.section(count)[:499], inline=False)
-    totallen = totallen + len(wikipedia.section(count))
+    embed.add_field(name=count, value=wpage.section(count)[:499], inline=False)
+    totallen = totallen + len(wpage.section(count))
   if len(wpage.images)>=1:
     embed.set_thumbnail(url = wpage.images[0])
   if len(wpage.images)>=2:
