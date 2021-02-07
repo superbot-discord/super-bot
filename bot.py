777f50fe9c1101fd799a9cf3653242c57b06436d
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-07 12:03:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -854,7 +854,7 @@ async def calc(ctx, *, arg = None):
 async def ping(ctx, *, text = None):
   now1 = datetime.now()
   message = await ctx.send("Pong!")
-  mcs = str((datetime.now() - now1).microseconds+(datetime.now() - now1).seconds*1000000)
+  mcs = str((datetime.now() - now1).microseconds+((datetime.now() - now1).total_seconds)%60)
   await message.edit(content="Pong! "+mcs+" microseconds")
 
 @bot.command()
