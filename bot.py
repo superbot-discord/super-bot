56e6a3baf3fe3cefcdacff696f37cbf694d20a8f
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-08 00:44:58 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from datetime import datetime, date, timedelta
 from selenium.webdriver.common.by import By
 from pygoogletranslation import Translator
 from discord_webhook import DiscordWebhook
+import matplotlib.pyplot as plt
 from pdf2image import convert_from_path
 from PIL import ImageDraw, ImageFilter
 from PyDictionary import PyDictionary
@@ -465,8 +466,16 @@ async def covid(ctx, *, country="world"):
     embed.add_field(name="Serious Cases", value=needrow.findAll('td')[9].string, inline=True)
     embed.add_field(name="Cases/1M", value=needrow.findAll('td')[10].string, inline=True)
     embed.add_field(name="Deaths/1M", value=needrow.findAll('td')[11].string, inline=True)
+    y = np.array([int(needrow.findAll('td')[8].string.replace(",",""))-int(needrow.findAll('td')[9].string.replace(",","")), int(needrow.findAll('td')[9].string.replace(",","")), int(needrow.findAll('td')[6].string.replace(",","")), int(needrow.findAll('td')[4].string.replace(",",""))])
+    mylabels = ["Active (Mild)", "Active (Serious)", "Recovered", "Died"]
+    mycolors = ["#4287F5", "#FF5252", "#CAFF99", "#A1A1A1"]
+    plt.pie(y, labels = mylabels, colors = mycolors)
+    plt.legend()
+    plt.savefig("piechart.png", transparent=True)
+    file = discord.File("piechart.png", filename="piechart.png")
+    embed.set_image(url="attachment://piechart.png")
     if country != "world":
-      embed.add_field(name="Recovered/1M", value=str(int(needrow.findAll('td')[6].string)/int(needrow.findAll('td')[14].string)*1000000), inline=True)
+      embed.add_field(name="Recovered/1M", value=str(int(needrow.findAll('td')[6].string.replace(",",""))/int(needrow.findAll('td')[14].string.replace(",",""))*1000000), inline=True)
       embed.add_field(name="Total Tests", value=needrow.findAll('td')[12].string, inline=True)
       embed.add_field(name="Tests/1M", value=needrow.findAll('td')[13].string, inline=True)
     await ctx.send(embed=embed)
