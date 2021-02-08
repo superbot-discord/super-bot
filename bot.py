d41d2ea282ad004093a713099380a283ea268976
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-09 00:13:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -468,7 +468,7 @@ async def pie(ctx, title, numbers, labels):
     mycolors.append(cmaphsv(count/len(numlist)))
   plt.pie(y, labels = labels.split(","), colors=mycolors, autopct=lambda pct: func(pct, y), textprops = {'color':"w"})
   plt.legend(loc="lower right")
-  plt.set_title(title)
+  plt.title(title)
   plt.savefig("piechart.png", transparent=True)
   plt.clf()
   file = discord.File("piechart.png")
