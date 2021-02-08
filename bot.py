03d6d4084c593181d62308f189e7e27dcc9357be
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-09 07:49:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,18 +89,18 @@ async def on_message(message):
   if banned_ids.count(message.author.id)==0 and message.content.startswith("=") and message.content.startswith("==")==False:
     await bot.process_commands(message)
   elif message.content.startswith("="):
-    await message.channel.send("You are banned from the bot. Reason: "+banned_text[banned_id.index(message.author.id)])
+    await message.channel.send("You are banned from the bot. Reason: "+banned_text[banned_ids.index(message.author.id)])
 
 @bot.command()
 async def botban(ctx, user : discord.User, *, text="No reason was provided"):
-  if ctx.author.id == 687474789342117900:
+  if ctx.author.id == 687474789342117900 and banned_ids.cound(user.id) == 0:
     banned_ids.append(user.id)
     banned_text.append(text)
     await ctx.send("Banned user from using the bot.")
 
 @bot.command()
 async def botunban(ctx, user : discord.User):
-  if ctx.author.id == 687474789342117900:
+  if ctx.author.id == 687474789342117900 and banned_ids.cound(user.id) == 1:
     banned_text.remove(banned_text[banned_ids.index(user.id)])
     banned_ids.remove(user.id)
     await ctx.send("Unbanned user from using the bot.")
