935d2643d2c6344f46f5838813230116ba8b69be
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-08 20:56:08 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,21 +91,21 @@ async def on_message(message):
     await message.channel.send("You are banned from the bot. Reason: "+banned_text[banned_id.index(message.author.id)])
 
 @bot.command()
-async def botban(ctx, user = discord.User, *, text="No reason was provided"):
+async def botban(ctx, user : discord.User, *, text="No reason was provided"):
   if ctx.author.id == 687474789342117900:
     banned_ids.append(user.id)
     banned_text.append(text)
     await ctx.send("Banned user from using the bot.")
 
 @bot.command()
-async def botunban(ctx, user = discord.User):
+async def botunban(ctx, user : discord.User):
   if ctx.author.id == 687474789342117900:
     banned_text.remove(banned_text[banned_ids.index(user.id)])
     banned_ids.remove(user.id)
     await ctx.send("Unbanned user from using the bot.")
 
 @bot.command()
-async def botadmin(ctx, user = discord.User):
+async def botadmin(ctx, user : discord.User):
   if ctx.author.id == 687474789342117900:
     bot_admins.append(user.id)
     await ctx.send("Added user as bot admin.")
