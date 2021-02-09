6548b93a4cc6c0d1abcaa41cee60f2e0b3345352
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-09 21:55:08 +0800

    Update botpycalc.py

--- botpycalc.py
+++ botpycalc.py
@@ -11,7 +11,7 @@ from math import *
 import subprocess
 import re
 
-def botpython(script):
+async def botpython(script):
   python_pattern = re.compile(r'^\`\`\`(py|python)?\n[\s\S]*\`\`\`$')
   match = python_pattern.fullmatch(script)
   if match:
