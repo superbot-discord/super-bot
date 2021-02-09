c0a43983f82fe20a37d667ed66a2ebcd01ac5664
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-09 21:49:53 +0800

    Update botpycalc.py

--- botpycalc.py
+++ botpycalc.py
@@ -2,6 +2,7 @@ from discord import Webhook, RequestsWebhookAdapter
 from discord_webhook import DiscordWebhook
 from discord.ext.commands import *
 from discord.ext import commands
+import discord
 from math import *
 from cmath import *
 import random as ra
