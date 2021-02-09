441f3f66dd7ed1b2bb14e4922527c41b1ad2eb39
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-09 08:34:50 +0800

    Update botbasic.py

--- botbasic.py
+++ botbasic.py
@@ -2,6 +2,7 @@ from discord import Webhook, RequestsWebhookAdapter
 from discord_webhook import DiscordWebhook
 from discord.ext.commands import *
 from discord.ext import commands
+from bot import *
 
 @bot.command()
 async def nick(ctx, *, newnick):
