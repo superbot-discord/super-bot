f130850f0af0de82ffd959d0cdf8a05828d75e74
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-10 09:16:40 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -149,7 +149,7 @@ async def engrave(ctx, product = "list", *, text = "Your text goes here."):
 
 @bot.command()
 async def python(ctx, *, script):
-  output = asyncio.run(botpython(script))
+  output = await botpython(script)
   await ctx.send(output)
 
 @bot.command()
