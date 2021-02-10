fde1ad6ea55f1e6895e7a2c43b5d0b24e6048879
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-10 09:02:50 +0800

    Update botpycalc.py

--- botpycalc.py
+++ botpycalc.py
@@ -22,7 +22,7 @@ async def botpython(script):
   file.close()
   proc = subprocess.Popen(['python', 'program.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   try:
-    output = await str(proc.communicate(timeout = 1)[0])
+    output = str(await proc.communicate(timeout = 1)[0])
     output = output.lstrip("'b(").rstrip("\\n'").replace("\n", f"\n")
   except subprocess.TimeoutExpired:
     await proc.kill()
