787ed9e60f4688368e190c3acf5def0d6fd9b21e
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-11 09:21:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from discord import Webhook, RequestsWebhookAdapter
 from datetime import datetime, date, timedelta
 from selenium.webdriver.common.by import By
-from pygoogletranslation import Translator
 from discord_webhook import DiscordWebhook
 import matplotlib.pyplot as plt
 from pdf2image import convert_from_path
@@ -227,28 +226,12 @@ async def barv(ctx, title, numbers, label):
 
 @bot.command()
 async def translate(ctx, langinput = "list", *, text = "Sample text"):
-  if langinput == "list" or langinput == "all":
-    embed1 = discord.Embed(description = f"**List of Language Input (Abbreviations)**\n`"+"` `".join(list(langdict.keys()))+f"`\n\n**List of Language Input (Full Names)**\n`"+"` `".join(list(langdict.values())))
-    embed2 = discord.Embed(description = f"**List of Language Output (Abbreviations)**\n`"+"` `".join(list(srclangdict.keys()))+f"`\n\n**List of Language Output (Full Names)**\n`"+"` `".join(list(srclangdict.values()))+"`")
-    await ctx.send(embed=embed1)
-    await ctx.send(embed=embed2)
+  output = bottranslate(langinput, text)
+  if type(output)==list:
+    await ctx.send(embed=output[0])
+    await ctx.send(embed=output[1])
   else:
-    if langinput.count(",")==1:
-      lang = langinput.split(",")[0]
-      fromlang = langinput.split(",")[1]
-      if list(srclangdict.values()).count(fromlang) == 1:
-        fromlang = list(srclangdict.keys())[list(srclangdict.values()).index(fromlang)]
-    else:
-      fromlang = "auto"
-      lang = langinput
-    if list(langdict.values()).count(lang) == 1:
-      lang = list(langdict.keys())[list(langdict.values()).index(lang)]
-    msg = await ctx.send("Translating **"+text+"** to "+langdict[lang])
-    translation = translatorvar.translate(text, src=fromlang, dest=lang)
-    try:
-      await msg.edit(content = "**Translation from "+srclangdict[fromlang]+" to "+langdict[lang]+f":**\n"+translation.text.replace("u003c", "<").replace("u003e", ">").replace("u0026", "&"))
-    except:
-      await ctx.send("Language not found! Please use `=translate list` to get a list of languages.")
+    await ctx.send(output)
 
 @bot.command()
 async def transparent(ctx, alpha = 128):
@@ -502,33 +485,8 @@ async def definition(ctx, *, word):
 
 @bot.command()
 async def wiki(ctx, *, query):
-  totallen = 0
-  try:
-    desc = wikipedia.summary(query)[:2047]
-    totallen = totallen + len(wikipedia.summary(query)) + len(desc) + len(query)
-    wpage = wikipedia.page(title=query, auto_suggest=True, redirect=True, preload=False)
-    embed = discord.Embed(title=query, url="https://en.wikipedia.org/wiki/"+wpage.title.replace(" ","_"), description=desc)
-    print(wpage.sections)
-    counter = 0
-    for count in wpage.sections:
-      if counter >=4 or totallen + len(wpage.section(count)) >= 6000:
-        break
-      if len(wpage.section(count))!=0:
-        embed.add_field(name=count, value=wpage.section(count)[:499], inline=False)
-        totallen = totallen + len(wpage.section(count))
-        counter = counter + 1
-    if len(wpage.images)>=1:
-      embed.set_thumbnail(url = wpage.images[0])
-    if len(wpage.images)>=2:
-      embed.set_image(url = wpage.images[1])
-    await ctx.send(embed = embed)
-  except:
-    results = wikipedia.search(query, results=20, suggestion=False)
-    desc = "**Please make one of these searches:**"
-    for count in results:
-      desc = desc + "`"+str(count)+"` "
-    embed = discord.Embed(title=query, description=desc)
-    await ctx.send(embed = embed)
+  embed = botwiki(query)
+  await ctx.send(embed=embed)
   
 @bot.command()
 async def purgereactions(ctx, messages, emoji: discord.Emoji = None):
