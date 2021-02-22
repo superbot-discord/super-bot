0b0bb70c720288f90bd02e383465dc4cd26bc420
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-22 11:11:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ from botwebinfo import *
 from botengrave import *
 from botpycalc import *
 from botbasic import *
+from botembed import *
 from botplot import *
 from botinfo import *
 
@@ -578,50 +579,7 @@ async def simpembed, *, text):
 
 @bot.command()
 async def embed(ctx,*,text):
-  textlist=text.splitlines()
-  if len(textlist) == 1:
-    embed=discord.Embed(title=textlist[0])
-  elif len(textlist) == 2:
-    embed=discord.Embed(title=textlist[0], url=textlist[1])
-    textlist.remove(textlist[0])
-  elif len(textlist) == 3 or (len(textlist) == 4 and (textlist[3] == "" or textlist[3] == " ")):
-    embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"))
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-  elif len(textlist) == 4:
-    embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"), color=int(textlist[3]))
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-  textlist.remove(textlist[0])
-  if len(textlist) == 1:
-    embed.set_author(name=textlist[0])
-  elif len(textlist) == 2:
-    embed.set_author(name=textlist[0], url=textlist[1])
-    textlist.remove(textlist[0])
-  elif len(textlist) == 3:
-    embed.set_author(name=textlist[0], url=textlist[1], icon_url=textlist[2])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-  textlist.remove(textlist[0])
-  if len(textlist) == 1:
-    embed.set_footer(text=textlist[0])
-    textlist.remove(textlist[0])
-  if len(textlist) == 1:
-    embed.set_thumbnail(url=textlist[0])
-    textlist.remove(textlist[0])
-  if len(textlist) == 1:
-    embed.set_image(url=textlist[0])
-    textlist.remove(textlist[0])
-  for count in range(0,len(textlist)//3):
-    if textlist[2].lower()=="y" or textlist[2].lower()=="yes" or textlist[2].lower()=="true" or textlist[2].lower()=="1":
-      inl=True
-    else:
-      inl=False
-    embed.add_field(name=textlist[0], value=textlist[1].replace("{{{newline}}}","\n"), inline=inl)
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+  embed = botembed(text)
   await ctx.send(embed=embed)
 
 @bot.command()
