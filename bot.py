528a558fdbc29b808b36fbadab150d72ca8473ea
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-22 10:36:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def on_message(message):
   match = verify_pattern.fullmatch(message.content)
   print(message.channel.id)
   print(match)
-  print(message.author.roles.count(message.guild.get_role(810729029790597190))
+  print(message.author.roles.count(message.guild.get_role(810729029790597190)))
   if message.channel.id == 811562994151850024 and match == False and message.author.roles.count(message.guild.get_role(810729029790597190)) == 0:
     await message.channel.send(f"Invalid verification format! Please double check the format and try again.\n**Original Content: **"+message.content, delete_after=10)
     try:
