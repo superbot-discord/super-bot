b9f85907906eb2217a8f6ec85dbe8df54a2c3ff2
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-24 08:52:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
+from discord_slash import SlashCommand, SlashContext
 from discord import Webhook, RequestsWebhookAdapter
 from datetime import datetime, date, timedelta
 from selenium.webdriver.common.by import By
 from discord_webhook import DiscordWebhook
 from pdf2image import convert_from_path
-from discord_slash import SlashCommand
 import selenium.common.exceptions
 from discord.ext import commands
 import matplotlib.pyplot as plt
