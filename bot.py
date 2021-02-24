95cbc708cc57777094da9984180bbf3f78d085a4
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-24 13:15:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -164,7 +164,7 @@ async def python(ctx, *, script):
 @bot.command()
 async def regex(ctx, regularexp, *, text):
   theregex = re.compile(r"(?P<LargestCapturingGroup>"+regularexp+")")
-  newtext = re.sub(theregex, r"**\LargestCapturingGroup**", text)
+  newtext = re.sub(theregex, r"**\g<LargestCapturingGroup>**", text)
   matches = len(re.findall(theregex, text))
   if matches == 1:
     ti = "There was 1 occurence."
