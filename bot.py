c340b52537157991a8c634c0ed7683964093651f
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-26 16:05:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ from botinfo import *
 
 banned_ids = []
 banned_text = []
-bot_admins = [687474789342117900, 457805013474082817, 757431801487556748]
+bot_admins = [687474789342117900]
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("="), intents=discord.Intents.all())
 slash = SlashCommand(bot)
 client = discord.Client()
