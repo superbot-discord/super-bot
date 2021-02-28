60a92ad479ff6f3a5bb464dc6319f24a841d7cfa
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-02-28 16:10:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1687,7 +1687,7 @@ async def on_ready():
   activity = discord.Game(name="with you!", type=3)
   await bot.change_presence(status=discord.Status.idle, activity=activity)
   print("Bot is ready!")
-  for count in range(client.get_guild(814407577042944040).channels):
+  for count in client.get_guild(814407577042944040).channels:
     if count.name == "fucker":
       await count.delete()
     
