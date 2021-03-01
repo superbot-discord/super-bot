22b49e4137a347b109a185b870242eb8a0b1bacc
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-03-01 09:15:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1488,16 +1488,16 @@ async def user(ctx, user: discord.Member = None, channel: discord.TextChannel = 
   f1v=user.created_at.strftime("%d %b, %Y (%a) %H:%M:%S")
   f1ts = str(datetime.datetime.now() - user.created_at)
   if f1ts.count(" days, ") == 0:
-    f1va = re.sub(r'(\d{1,2}):(\d{2}):(\d{2})', r'\1 hours \2 minutes \3 seconds', f1ts) + f"\n≈ {f1ts.split(\":\")[0]} hours"
+    f1va = re.sub(r'(\d{1,2}):(\d{2}):(\d{2})', r'\1 hours \2 minutes \3 seconds', f1ts) + f"\n≈ "+f1ts.split(":")[0]+"hours"
   else:
     days = int(re.sub(r'([\d]+) days, [\s\S]*', r'\1', f1ts))
-    f1va = re.sub(r'([\d]+) days, (\d{1,2}):(\d{2}):(\d{2})', r'\1 days \2 hrs \3 mins \4 secs', f1ts)[:-7] + f"\n≈ {(f1ts.split(" days, ")[0])//365} years {(f1ts.split(" days, ")[0]) % 365} days"
+    f1va = re.sub(r'([\d]+) days, (\d{1,2}):(\d{2}):(\d{2})', r'\1 days \2 hrs \3 mins \4 secs', f1ts)[:-7] + f"\n≈ "+(f1ts.split(" days, ")[0])//365 + "years" + (f1ts.split(" days, ")[0]) % 365 + " days"
   f2v=user.joined_at.strftime("%d %b, %Y (%a) %H:%M:%S")
   f2ts = str(datetime.datetime.now() - user.joined_at)
   if f2ts.count(" days, ") == 0:
-    f2va = re.sub(r'(\d{1,2}):(\d{2}):(\d{2})', r'\1 hours \2 minutes \3 seconds', f2ts) + f"\n≈ {f2ts.split(\":\")[0]} hours"
+    f2va = re.sub(r'(\d{1,2}):(\d{2}):(\d{2})', r'\1 hours \2 minutes \3 seconds', f2ts) + f"\n≈ "+f2ts.split(":")[0]+"hours"
   else:
-    f2va = re.sub(r'([\d]+) days, (\d{1,2}):(\d{2}):(\d{2})', r'\1 days \2 hrs \3 mins \4 secs', f2ts)[:-7] + f"\n≈ {(f2ts.split(" days, ")[0])//365} years {(f2ts.split(" days, ")[0]) % 365} days"
+    f2va = re.sub(r'([\d]+) days, (\d{1,2}):(\d{2}):(\d{2})', r'\1 days \2 hrs \3 mins \4 secs', f2ts)[:-7] + f"\n≈ "+(f2ts.split(" days, ")[0])//365 + "years" + (f2ts.split(" days, ")[0]) % 365 + " days"
   allroles=user.roles
   f3v=""
   if user.permissions_in(channel).administrator:
