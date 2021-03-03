faa5efa7c9230b574be7c557bbfee3adc75961b3
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-03-03 17:27:43 +0800

    Update botembed.py

--- botembed.py
+++ botembed.py
@@ -34,7 +34,10 @@ def botembed(text):
     textlist.remove(textlist[0])
     textlist.remove(textlist[0])
   elif len(textlist) >= 4:
-    embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"), color=int(textlist[3]))
+    try:
+      embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"), color=int(textlist[3]))
+    except:
+      embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"))
     textlist.remove(textlist[0])
     textlist.remove(textlist[0])
     textlist.remove(textlist[0])
