d01e65513375435fff2923fe921431bc122de947
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-03-14 16:20:15 +0800

    Update botembed.py

--- botembed.py
+++ botembed.py
@@ -68,76 +68,61 @@ def botsimpembed(text):
     embed=discord.Embed(title=textlist[0])
   elif len(textlist) == 2:
     embed=discord.Embed(title=textlist[0], description=textlist[1].replace("{{{newline}}}",f"\n"))
-    textlist.remove(textlist[0])
-  textlist.remove(textlist[0])
+    textlist = textlist[1:]
+  textlist = textlist[1:]
   for count in range(0,len(textlist)//3):
     if textlist[2].lower()=="y" or textlist[2].lower()=="yes" or textlist[2].lower()=="true" or textlist[2].lower()=="1":
       inl=True
     else:
       inl=False
     embed.add_field(name=textlist[0], value=textlist[1].replace("{{{newline}}}",f"\n"), inline=inl)
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[3:]
   return embed
 
 def botembed(text):
   textlist=text.splitlines()
   if len(textlist) == 1 and len(textlist[0].replace(" ", ""))!=0:
     embed=discord.Embed(title=textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[1:]
   elif len(textlist) == 2 and len(textlist[0].replace(" ", ""))!=0 and len(textlist[1].replace(" ", ""))!=0:
     embed=discord.Embed(title=textlist[0], url=textlist[1])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[2:]
   elif (len(textlist) == 3 or (len(textlist) == 4 and len(textlist[3].replace(" ", ""))==0)):# and len(textlist[0].replace(" ", ""))!=0 and len(textlist[1].replace(" ", ""))!=0 and len(textlist[2].replace(" ", ""))!=0:
     embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"))
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-  elif len(textlist) >= 4 and len(textlist[0].replace(" ", ""))!=0 and len(textlist[3].replace(" ", ""))!=0:# and len(textlist[2].replace(" ", ""))!=0 and len(textlist[3].replace(" ", ""))!=0:
+    textlist = textlist[3:]
+  elif len(textlist) >= 4 and len(textlist[0].replace(" ", ""))!=0:# and len(textlist[3].replace(" ", ""))!=0:# and len(textlist[2].replace(" ", ""))!=0 and len(textlist[3].replace(" ", ""))!=0:
     try:
       embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"), color=int(textlist[3]))
-      textlist.remove(textlist[0])
-      textlist.remove(textlist[0])
-      textlist.remove(textlist[0])
+      textlist = textlist[4:]
     except:
       embed=discord.Embed(title=textlist[0], url=textlist[1], description=textlist[2].replace("{{{newline}}}","\n"))
-      textlist.remove(textlist[0])
-      textlist.remove(textlist[0])
-      textlist.remove(textlist[0])
+      textlist = textlist[3:]
   if len(textlist) == 1 and len(textlist[0].replace(" ", ""))!=0:
     embed.set_author(name=textlist[0])
     textlist.remove(textlist[0])
   elif len(textlist) == 2 and len(textlist[0].replace(" ", ""))!=0 and len(textlist[1].replace(" ", ""))!=0:
     embed.set_author(name=textlist[0], url=textlist[1])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[2:]
   elif len(textlist) >= 3 and len(textlist[0].replace(" ", ""))!=0 and len(textlist[2].replace(" ", ""))!=0:# and len(textlist[2].replace(" ", ""))!=0:
     embed.set_author(name=textlist[0], url=textlist[1], icon_url=textlist[2])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[3:]
   if len(textlist) == 1 and len(textlist[0].replace(" ", ""))!=0:
     embed.set_footer(text=textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[1:]
   elif len(textlist) == 2 and len(textlist[0].replace(" ", ""))!=0:# and len(textlist[1].replace(" ", ""))!=0:
     embed.set_footer(text=textlist[0], icon_url=textlist[1])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[2:]
   if len(textlist) == 1 and len(textlist[0].replace(" ", ""))!=0:
     embed.set_thumbnail(url=textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[1:]
   if len(textlist) == 1 and len(textlist[0].replace(" ", ""))!=0:
     embed.set_image(url=textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[1:]
   for count in range(0, len(textlist)//3):
     if textlist[2].lower()=="y" or textlist[2].lower()=="yes" or textlist[2].lower()=="true" or textlist[2].lower()=="1":
       inl=True
     else:
       inl=False
     embed.add_field(name=textlist[0], value=textlist[1].replace("{{{newline}}}",f"\n"), inline=inl)
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
-    textlist.remove(textlist[0])
+    textlist = textlist[3:]
   return embed
