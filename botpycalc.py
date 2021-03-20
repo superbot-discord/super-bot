5f8b61639a827adcc617a94c45a32db371c65bd0
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-03-20 15:47:45 +0800

    Update botpycalc.py

--- botpycalc.py
+++ botpycalc.py
@@ -82,7 +82,6 @@ def botcalc(arg : str):
     arg=arg.replace("√(","sqrt(")
     arg=arg.replace("pi",str(pi))
     arg=arg.replace(",","")
-    arg=arg.replace(".","")
     #arg=arg.replace("e",str(e))
     if arg.count("=")==0 or arg.count("==")!=0 or arg.count("!=")!=0 or arg.count(">=")!=0 or arg.count("<=")!=0 or arg.count(">")!=0 or arg.count("<")!=0 or arg.count("and")!=0 or arg.count("or")!=0 or arg.count("not")!=0:
       lcls = locals()
