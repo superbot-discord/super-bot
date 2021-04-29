50a801a3c4d942c85546b6c91a088d628429fba8
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-04-29 17:48:00 +0800

    Update botwebinfo.py

--- botwebinfo.py
+++ botwebinfo.py
@@ -26,16 +26,16 @@ def botunscramble(text):
   soup=BeautifulSoup(r.content, features="html.parser")
   everything = soup.findAll('a', target="_blank")
   length = 99999
-  output = discord.Embed(title=f"You used the word {text}.")
+  output = discord.Embed(title=f"Unscrambled results for {text}")
+  content = ""
   for count in everything[:-7]:
     formatted = count.text.rstrip(" ").replace(f"\n","")
     if length != len(formatted):
       length = len(formatted)
-      output.add_field(name=str(length)+"-letters", value = formatted)
+      output.add_field(name=str(length)+"-letters", value = content)
+      content = ""
     elif len(formatted) >= 0:
-     output += formatted
-  if len(output.replace(" ", "").replace(f"\n", "")) == 0:
-    output = "No results!"
+     content += f"`{formatted}` "
   return output
 
 def botminecraft(item):
