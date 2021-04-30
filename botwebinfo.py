9c78f78d1031e02a5e349b1e4657afa3fa7327b1
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-04-30 20:57:46 +0800

    Update botwebinfo.py

--- botwebinfo.py
+++ botwebinfo.py
@@ -28,7 +28,7 @@ def botunscramble(text):
   output = discord.Embed(title=f"Unscrambled results for {text}")
   for count in everything[0:-7]:
     formatted = count.text.rstrip(" ").replace(f"\n","")
-    if len(output)+len(formatted)>=5998:
+    if len(output)+len(formatted)>=5987:
       content += "…"
       break
     if count == everything[0]:
