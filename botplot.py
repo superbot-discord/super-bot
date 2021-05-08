be61f79e2c4e788471aec9e912031e7f89fe6063
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-05-08 21:23:49 +0800

    Update botplot.py

--- botplot.py
+++ botplot.py
@@ -1,10 +1,71 @@
+from table2ascii import table2ascii, PresetStyle
 import matplotlib.pyplot as plt
 import numpy as np
+from art import *
 
 cmaphsv = plt.cm.hsv
 def func(pct, allvals):
   absolute = int(pct/100*np.sum(allvals))
   return "{:d} ({:.1f}%)".format(absolute, int(pct))
+
+def botascii(text):
+  output = text2art(text,"cybermedium") + f"\n" + text2art(text,"big")+f"\n" + text2art(text,"future_1")
+  file = open("ascii.txt", "w")
+  file.write(output)
+  file.close()
+
+def bottable(text):
+  splitted = text.split(f"\n")
+  if len(splitted) == 1:
+    splitted.insert(0, "")
+    splitted.insert(0, "")
+  header = splitted[0]
+  footer = splitted[1]
+  everythingelse = splitted[2:len(splitted)]
+  if "|||" in header:
+    rawstyle = re.sub(r"([\w]*?)\|\|\|([\s\S]*)", r"\1", header)
+    header = re.sub(r"([\w]*?)\|\|\|([\s\S]*)", r"\2", header)
+  else:
+    rawstyle = ""
+  if header.startswith("$F$"):
+    first_col_heading = True
+    header = header.lstrip("$F$")
+  else:
+    first_col_heading = False
+  if header.startswith("$L$"):
+    last_col_heading = True
+    header = header.lstrip("$L$")
+  else:
+    last_col_heading = False
+  if rawstyle.replace(" ", "") == "":
+    style = PresetStyle.double_thin_compact
+  else:
+    try:
+      style = eval("PresetStyle."+rawstyle)
+    except:
+      style = PresetStyle.double_thin_compact
+  try:
+    headers = header.split(",")
+    footers = footer.split(",")
+  except:
+    pass
+  rawbodies = everythingelse#.split(f"\n")
+  bodies = []
+  for count in rawbodies:
+    bodies.append(count.split(","))
+  try:
+    output = table2ascii(header=headers, footer=footers, body=bodies, style=style, first_col_heading=first_col_heading,  last_col_heading=last_col_heading)
+  except:
+    try:
+      output = table2ascii(footer=footers, body=bodies, style=style)
+    except:
+      try:
+        output = table2ascii(header=headers, body=bodies, style=style)
+      except:
+        try:
+          output = table2ascii(body=bodies, style=style)
+        except:
+          output = "Invalid syntax, please try again."
 
 def botsimpcolor(name):
   plt.clf()
