b53c8c9187592805465ff323222d47b981311ced
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-07-16 13:04:04 +0800

    Update botwebinfo.py

--- botwebinfo.py
+++ botwebinfo.py
@@ -112,7 +112,7 @@ def botminecraft(item):
     """
     results = soup.findAll("img")
     image = str(results[1])
-    image = re.sub(r'<img alt=".*?"( class="thumbimage")? decoding="async" height="[\d]*?" src="([\s\S]*?)" width="[\d]*?"/>', r'\2', image)
+    image = re.sub(r'<img alt="[\s\S]*?"( class="thumbimage")? (decoding="async")? height="[\d]*?" src="([\s\S]*?)" (width="[\d]*?")?/>', r'\3', image)
     #image = re.sub(r'(https:\/\/static.wikia.nocookie.net\/minecraft_gamepedia\/images\/[\S]*?\/[\S]*?\/[\w]*?\.[\w]{2,5}\/revision\/latest)\/scale-to-width-down\/\d{1,4}\?cb=(\d{5,30})', r'\1?cb=\2&format=original', image)
     embed.set_image(url = image)
     return embed
