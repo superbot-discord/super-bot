8dfbaa365c0dd57efe4ca6a014963edb687badaf
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-07-17 02:36:14 +0800

    Update botanimals.py

--- botanimals.py
+++ botanimals.py
@@ -3,9 +3,14 @@ import requests
 import re
 
 def botcat(number):
-  desc = ""
-  for count in range(0, number):
+  if number == 1:
     r=requests.get(f"https://dog.ceo/api/breeds/image/random")
     link=re.sub('{"message":"([\s\S]*?)","status":"success"}', r'\1', r.content.decode("utf-8"))
-    desc += link + "\n"
+    desc = "[Cat](" + link + ")"
+  else:
+    desc = ""
+    for count in range(1, number+1):
+      r=requests.get(f"https://dog.ceo/api/breeds/image/random")
+      link=re.sub('{"message":"([\s\S]*?)","status":"success"}', r'\1', r.content.decode("utf-8"))
+      desc += "[Cat " + str(count) + "](" + link + ")"
   return desc
