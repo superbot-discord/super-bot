4af82b14b1e69d08435fbb43e363575952249999
johann-lau <65586326+johann-lau@users.noreply.github.com>
2021-07-17 04:23:36 +0800

    Update botanimals.py

--- botanimals.py
+++ botanimals.py
@@ -7,11 +7,14 @@ def botdog(number):
   if number == 1:
     r=requests.get(f"https://dog.ceo/api/breeds/image/random")
     link=re.sub('{"message":"([\s\S]*?)","status":"success"}', r'\1', r.content.decode("utf-8"))
-    desc = f"[Dog]({link})"
+    open('dog.jpg', 'w').write(requests.get(link).content)
+    desc = [discord.File('dog.jpg')]
   else:
-    desc = ""
+    desc = []
     for count in range(1, number+1):
       r=requests.get(f"https://dog.ceo/api/breeds/image/random")
       link=re.sub('{"message":"([\s\S]*?)","status":"success"}', r'\1', r.content.decode("utf-8"))
-      desc += f"[Dog {count}]({link})  "
+      fname = 'dog' + str(count) + '.jpg'
+      open(fname, 'w').write(requests.get(link).content)
+      desc.append(discord.File(fname))
   return desc
