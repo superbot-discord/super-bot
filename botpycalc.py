6547c083ca4861fe9d5abc5c168a3b925ce16bfa
johann-lau <johannlau8888@gmail.com>
2021-09-12 19:31:51 +0800

    Fixed =base, documentation will come soon

--- botpycalc.py
+++ botpycalc.py
@@ -64,14 +64,16 @@ def botdefine(function : str, definition : str, argumentsraw : str):
     return "Add_Reaction"
 
 @commands.command()
-async def base(ctx, text, frombase, tobase):
+async def base(ctx, frombase : int, tobase : int, *, text):
   integer = 0
   for character in text:
     if character not in SY2VA:
       await ctx.send('Found unknown character!')
+      return
     value = SY2VA[character]
     if value >= frombase:
-      await ctx.send('Found digit outside base!')
+      await ctx.send(f'Found digit outside base! {value} is greater than {frombase}.')
+      return
     integer *= frombase
     integer += value
   VA2SY = dict(map(reversed, SY2VA.items()))
