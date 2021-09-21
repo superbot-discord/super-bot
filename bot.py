b675b0e2a2e8bdd1bee26d3992df6c4dd2feb45c
johann-lau <johannlau8888@gmail.com>
2021-09-21 19:44:07 +0800

    Fixed command handling

--- bot.py
+++ bot.py
@@ -198,10 +198,10 @@ async def on_message(message:discord.Message):
     await message.add_reaction("<:DownArrowSquare:864762633625534485>")
   elif message.channel.id in [805459414001778739, 805462208414089217, 880076327783370812]:
     await message.publish()
-    if banned_ids.count(message.author.id)==0 and message.content.startswith("=") and message.content.startswith("==")==False:
-      await bot_.process_commands(message)
-    elif message.content.startswith("="):
-      await message.channel.send("You are banned from the bot. Reason: "+banned_text[banned_ids.index(message.author.id)])
+  if banned_ids.count(message.author.id)==0 and message.content.startswith("=") and message.content.startswith("==")==False:
+    await bot_.process_commands(message)
+  elif message.content.startswith("="):
+    await message.channel.send("You are banned from the bot. Reason: "+banned_text[banned_ids.index(message.author.id)])
 
 @bot_.command(aliases=['sniper'])
 async def snipe(ctx, *, text = None):
