34d8e6d7bb76cd9cd2c04a0dd6056d089cc02e4d
johann-lau <johannlau8888@gmail.com>
2021-09-27 08:44:49 +0800

    Improved music

--- botdevelopment.py
+++ botdevelopment.py
@@ -28,6 +28,15 @@ async def leave(ctx, *, text=None):
   await ctx.send("Left the channel.")
 
 @commands.command()
+async def loop(ctx, *, text=None):
+  if ctx.voice_state.loop:
+    ctx.voice_state.loop = False
+    await ctx.send('Disabled loop.')
+  elif not ctx.voice_state.loop:
+    ctx.voice_state.loop = True
+    await ctx.send('Enabled loop.')
+
+@commands.command()
 @commands.cooldown(2, 10, commands.BucketType.user)
 async def patience(ctx, *, text=None):
   await ctx.send("Success!")
@@ -36,8 +45,17 @@ async def patience(ctx, *, text=None):
 async def patience_error(ctx, error):
   await ctx.send("This command is on cooldown! You can only use it twice per 10 seconds.")
 
+@commands.command(aliases=['continue', 'resume', 'paused'])
+async def pause(ctx, *, text=None):
+  if ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
+    ctx.voice_state.voice.pause()
+    await ctx.send("Paused the song.")
+  else:
+    ctx.voice_state.voice.resume()
+    await ctx.send("Resumed the song.")
+
 @commands.command()
-async def play(ctx, volume: typing.Optional[int]=100, *, song):
+async def play(ctx, volume: typing.Optional[int]=100, *, song="rickroll"):
   vc = vclients.get(ctx.guild, None)
   if not vc:
     vc = await ctx.author.voice.channel.connect()
@@ -46,12 +64,12 @@ async def play(ctx, volume: typing.Optional[int]=100, *, song):
     audio_source = discord.FFmpegPCMAudio('songs/rickroll.mp3', options=f'-filter:a "volume={volume/100}"')
   elif SequenceMatcher(None, song, "stickbug").ratio() > 0.7:
     audio_source = discord.FFmpegPCMAudio('songs/stickbug.mp3')
-  else:
+  elif len(ctx.message.attachments):
     random_play_id = random.randint(1000000,9999999)
     await ctx.message.attachments[0].save(f'music_{random_play_id}.mp3')
     audio_source = discord.FFmpegPCMAudio(f'music_{random_play_id}.mp3')
   vc.play(audio_source)
-  await ctx.send("Now playing the song.")
+  await ctx.send("Playing the song.")
 
 @commands.command(aliases=['selectmenu', 'menu', 'option', 'options'])
 async def select(ctx, *, text=None):
