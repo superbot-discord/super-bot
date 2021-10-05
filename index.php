5c87e549c16b2c3ac0df8a9133dda95d7af408b1
johann-lau <johannlau8888@gmail.com>
2021-10-05 15:05:42 +0800

    More PHP tests

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<h1>
-  Test
-</h1>
-<audio id="rickroll" src="songs/rickroll.mp3" autoplay></audio>
-<script>
-  alert("nice!")
-  document.getElementById("rickroll").play()
-</script>
+<html>
+
+<body>
+  <h1>
+    Test
+  </h1>
+  <audio id="rickroll" src="songs/rickroll.mp3" autoplay controls></audio>
+  <script>
+  alert("nice!");
+  document.getElementById("rickroll").play();
+  </script>
+</body>
+
+</html>
