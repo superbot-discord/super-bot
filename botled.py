154fbee2a7335dcc5584fc7653d3324bc64e5bf8
johann-lau <johannlau8888@gmail.com>
2021-10-09 17:06:14 +0800

    Tested two LED font modes

--- botled.py
+++ botled.py
@@ -11,11 +11,15 @@ async def lcd(ctx, *, text):
   os.remove('output.png')
 
 @commands.command()
-async def led(ctx, *, text):
-  sizes = font_led.getsize_multiline(text, spacing=20)
+async def led(ctx, mode: typing.Optional[typing.Literal['bold', 'regular']] = 'regular', *, text):
+  if mode == 'bold':
+    current_font = font_led_bold
+  else:
+    current_font = font_led
+  sizes = current_font.getsize_multiline(text, spacing=20)
   image = Image.new("RGBA", (sizes[0], sizes[1]+15), color="#300000FF")
   draw = ImageDraw.Draw(image)
-  draw.multiline_text((0, 3), text, font=font_led, fill="#D83030FF", spacing=20)
+  draw.multiline_text((0, 3), text, font=current_font, fill="#D83030FF", spacing=20)
   image.save('output.png')
   await ctx.send(file=discord.File('output.png'))
   os.remove('output.png')
--- shared.py
+++ shared.py
@@ -34,8 +34,9 @@ f = open('database.json', 'r')
 db = json.loads(f.read())
 f.close()
 
-font_led = ImageFont.truetype("fonts/led.ttf", 50)
-font_lcd = ImageFont.truetype("fonts/lcd.ttf", 50)
+font_led      = ImageFont.truetype("fonts/led.ttf", 50)
+font_led_bold = ImageFont.truetype("fonts/led_bold.ttf", 50)
+font_lcd      = ImageFont.truetype("fonts/lcd.ttf", 50)
 
 func            =lambda pct, allvals   : "{:d} ({:.1f}%)".format(int(pct/100*np.sum(allvals)), round(pct, 1))
 botadmin        =lambda context        : context.author.id in db["botadmins"]
